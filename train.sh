c0f7552cad478c88c29a4fb59efb9d353d9f6acd
Kerem Turgutlu <keremturgutlu@gmail.com>
2024-01-19 13:09:54 +0300

    train.py

--- train.sh
+++ train.sh
@@ -1,12 +1,10 @@
 # Compare LORA and QLORA on Alpaca dataset with same effective batch size ~32, lr sched, and lr.
-
+# Reference for some hyperparams: https://arxiv.org/abs/2305.14314
 # LORA (pure bf16)
 # https://wandb.ai/answerdotai/fsdp/runs/gb34o6p4?workspace=user-k-answer-ai
 # NOTE: Loss curve is flat - 1) use lower lr ? 2) start immediate annealing get_cosine_one_cycle_scheduler(..., min_lr_fraction=0.0)
 python train.py \
 --model_name meta-llama/Llama-2-7b-hf \
---lr 1e-5 \
---lr_scheduler cosine \
 --gradient_accumulation_steps 2 \
 --batch_size 8 \
 --context_length 512 \
@@ -15,13 +13,12 @@ python train.py \
 --use_gradient_checkpointing False \
 --use_cpu_offload False \
 --log_to wandb \
---dataset alpaca
+--dataset alpaca \
+--verbose false
 
 # QLORA (pure bf16)
 python train.py \
 --model_name meta-llama/Llama-2-7b-hf \
---lr 1e-5 \
---lr_scheduler cosine \
 --gradient_accumulation_steps 2 \
 --batch_size 8 \
 --context_length 512 \
@@ -30,14 +27,13 @@ python train.py \
 --use_gradient_checkpointing False \
 --use_cpu_offload False \
 --log_to wandb \
---dataset alpaca_sample
+--dataset alpaca \
+--verbose false
 
 # QLORA (autocast bf16)
 python train.py \
 --model_name meta-llama/Llama-2-7b-hf \
 --mixed_precision_mode autocast_bf16 \
---lr 1e-5 \
---lr_scheduler cosine \
 --gradient_accumulation_steps 2 \
 --batch_size 8 \
 --context_length 512 \
@@ -46,7 +42,8 @@ python train.py \
 --use_gradient_checkpointing False \
 --use_cpu_offload False \
 --log_to wandb \
---dataset alpaca_sample
+--dataset alpaca \
+--verbose false
 
 
 
