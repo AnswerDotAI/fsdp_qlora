c3e8ca13029aa3931303d0c849a1ac8835caef42
Kerem Turgutlu <keremturgutlu@gmail.com>
2024-01-29 12:50:13 +0000

    exp notes

--- benchmarking/small_gpu_benchmarking.sh
+++ benchmarking/small_gpu_benchmarking.sh
@@ -51,7 +51,7 @@ python train.py --batch_size 4 --model_name meta-llama/Llama-2-13b-hf --context_
 # # Test low memory
 # python train.py --batch_size 1 --model_name meta-llama/Llama-2-7b-hf --context_length 16 --use_gradient_checkpointing True --train_type qlora --low_memory True
 # # This works now. Custom QLORA nn.module, no changes needed in bnb.
-# python train.py --batch_size 1 --model_name meta-llama/Llama-2-70b-hf --context_length 2 --use_gradient_checkpointing True --train_type qlora --low_memory True
+# python train.py --batch_size 1 --model_name meta-llama/Llama-2-70b-hf --context_length 1 --use_gradient_checkpointing True --train_type qlora --low_memory True
 
 # This is theoretically not possible:
 # python train.py --batch_size 128 --model_name codellama/CodeLlama-34b-hf --context_length 256 --use_gradient_checkpointing True --train_type lora
@@ -62,7 +62,9 @@ python train.py --batch_size 6 --model_name codellama/CodeLlama-34b-hf --context
 python train.py --batch_size 1 --use_ddp True --model_name codellama/CodeLlama-34b-hf --context_length 256 --use_gradient_checkpointing True --train_type qlora
 # FIXME: torch.multiprocessing.spawn.ProcessExitedException: process 1 terminated with signal SIGKILl. Needs more CPU memory than 88GB?
 python train.py --batch_size 1 --model_name codellama/CodeLlama-34b-hf --context_length 256 --use_gradient_checkpointing True --use_cpu_offload True --train_type lora
+# Qlora cpu offloading works as opposed to lora, likely due to smaller model size after quantization?
 python train.py --batch_size 10 --model_name codellama/CodeLlama-34b-hf --context_length 256 --use_gradient_checkpointing True --use_cpu_offload True --train_type qlora
+# Hangs after the model loading changes. Can't load without low_memory option.
 python train.py --batch_size 1 --model_name codellama/CodeLlama-34b-hf --context_length 256 --use_gradient_checkpointing False --train_type lora
 python train.py --batch_size 128 --model_name codellama/CodeLlama-34b-hf --context_length 256 --use_gradient_checkpointing False --train_type qlora
 python train.py --batch_size 128 --model_name codellama/CodeLlama-34b-hf --context_length 256 --use_gradient_checkpointing False --use_cpu_offload True --train_type lora
