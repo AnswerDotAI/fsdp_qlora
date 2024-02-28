3417720e3fbcf691c0487cc337b2f701a598e680
Kerem Turgutlu <keremturgutlu@gmail.com>
2024-02-28 09:10:21 +0000

    hqq experiments

---
+++ train_hqq_bench.sh
@@ -0,0 +1,55 @@
+# Full vs QLORA vs HQQ, batch size = 64
+
+# max batch size / gpu = 8 (38/40 GB)
+# 8 * 2 gpus * 4 grad accum  = 64
+export CUDA_VISIBLE_DEVICES=4,5
+python train.py \
+--world_size 2 \
+--master_port 12356 \
+--model_name meta-llama/Llama-2-7b-hf \
+--gradient_accumulation_steps 4 \
+--batch_size 8 \
+--context_length 512 \
+--precision bf16 \
+--train_type full \
+--use_gradient_checkpointing true \
+--use_cpu_offload false \
+--log_to wandb \
+--dataset alpaca \
+--verbose true
+
+# max batch size / gpu = 16 (28/40 GB)
+# 16 * 2 gpus * 2 grad accum  = 64
+export CUDA_VISIBLE_DEVICES=4,5
+python train.py \
+--world_size 2 \
+--master_port 12356 \
+--model_name meta-llama/Llama-2-7b-hf \
+--gradient_accumulation_steps 2 \
+--batch_size 16 \
+--context_length 512 \
+--precision bf16 \
+--train_type custom_qlora \
+--use_gradient_checkpointing true \
+--use_cpu_offload false \
+--log_to wandb \
+--dataset alpaca \
+--verbose true
+
+# max batch size / gpu = 32 (28/40 GB)
+# 32 * 2 gpus = 64
+export CUDA_VISIBLE_DEVICES=4,5
+python train.py \
+--world_size 2 \
+--master_port 12356 \
+--model_name meta-llama/Llama-2-7b-hf \
+--gradient_accumulation_steps 1 \
+--batch_size 32 \
+--context_length 512 \
+--precision bf16 \
+--train_type hqq_lora \
+--use_gradient_checkpointing true \
+--use_cpu_offload false \
+--log_to wandb \
+--dataset alpaca \
+--verbose true
