62d3ef6453ad4cac705056a87850459631c71b47
Kerem Turgutlu <keremturgutlu@gmail.com>
2024-05-09 09:19:10 +0000

    instruct zero, few, full results

--- experiments/orca_math/train_orca_math.sh
+++ experiments/orca_math/train_orca_math.sh
@@ -287,9 +287,29 @@
 # --save_model true \
 # --output_dir /workspace/models/llama-3-8b-instruct-orca-math-10k-hqq-qlora-plus-loftq-init
 
+# # 10k qdora instruct (hqq axis=1 for torchao kernel compat)
+# cd /workspace/git/fsdp_qlora && python train.py \
+# --train_type hqq_dora \
+# --model_name meta-llama/Meta-Llama-3-8B-Instruct \
+# --dataset orca_math_instruct \
+# --dataset_samples 10000 \
+# --batch_size 4 \
+# --context_length 2048 \
+# --gradient_accumulation_steps 2 \
+# --sharding_strategy full_shard \
+# --use_gradient_checkpointing true \
+# --reentrant_checkpointing true \
+# --use_cpu_offload false \
+# --use_activation_cpu_offload false \
+# --log_to wandb \
+# --verbose true \
+# --project_name "fsdp-quantized-ft-exps" \
+# --save_model true \
+# --output_dir /workspace/models/llama-3-8b-instruct-orca-math-10k-hqq-qdora-axis-1
+
 # 10k qdora instruct (hqq axis=1 for torchao kernel compat)
 cd /workspace/git/fsdp_qlora && python train.py \
---train_type hqq_dora \
+--train_type full \
 --model_name meta-llama/Meta-Llama-3-8B-Instruct \
 --dataset orca_math_instruct \
 --dataset_samples 10000 \
@@ -305,4 +325,4 @@ cd /workspace/git/fsdp_qlora && python train.py \
 --verbose true \
 --project_name "fsdp-quantized-ft-exps" \
 --save_model true \
---output_dir /workspace/models/llama-3-8b-instruct-orca-math-10k-hqq-qdora-axis-1
+--output_dir /workspace/models/llama-3-8b-instruct-orca-math-10k-full
