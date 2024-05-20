fdc3c7e19ca64c0eebdf2a9128a1a47dec1426e9
austinvhuang <austinh@alum.mit.edu>
2024-05-20 17:10:05 -0400

    add comment to profile.sh

--- profile.sh
+++ profile.sh
@@ -1,3 +1,15 @@
+# This generates uses export_stacks to generate profiling output
+# /tmp/profile_0.txt, /tmp/profile_1.txt, etc. (1 file per process.)
+#
+# Output files are generated using export_stacks(), note there are some
+# outstanding issues to be aware of:
+# https://github.com/pytorch/pytorch/issues/100253
+#
+# Profiling output files can be used with speedscope or other tools.
+#
+# For additional information, see:
+# https://pytorch.org/tutorials/recipes/recipes/profiler_recipe.html
+
 python train.py \
     --model_name meta-llama/Meta-Llama-3-8B \
     --train_type hqq_dora \
@@ -13,3 +25,4 @@ python train.py \
     --use_activation_cpu_offload False \
     --save_model False \
     --profiling_output /tmp/profile
+
