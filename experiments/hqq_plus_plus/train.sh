41ccbcdd6cd9b031932ba0ab08a40e77280eb513
root <keremturgutlu@gmail.com>
2024-07-17 10:40:36 +0000

    train 4/2 bit

--- experiments/hqq_plus_plus/train.sh
+++ experiments/hqq_plus_plus/train.sh
@@ -19,3 +19,22 @@ cd /workspace/git/fsdp_qlora && python train.py \
 --project_name "fsdp-quantized-ft-exps" \
 --save_model true \
 --output_dir /workspace/models/llama-3-8b-instruct-hqq-dora-plus-plus
+
+
+cd /workspace/git/fsdp_qlora && python train.py \
+--train_type hqq_dora \
+--model_name meta-llama/Meta-Llama-3-8B-Instruct \
+--dataset /workspace/data/hqq_plus_dataset \
+--batch_size 8 \
+--context_length 1024 \
+--gradient_accumulation_steps 2 \
+--sharding_strategy full_shard \
+--use_gradient_checkpointing true \
+--reentrant_checkpointing false \
+--use_cpu_offload false \
+--use_activation_cpu_offload false \
+--log_to stdout \
+--verbose true \
+--project_name "fsdp-quantized-ft-exps" \
+--save_model true \
+--output_dir /workspace/models/llama-3-8b-instruct-hqq-dora-plus-plus-4bit-gs128-2bit-qs64
