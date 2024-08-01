ee2ce9eb0f9f416e07e825ebd60bbf3e9364e0cb
root <keremturgutlu@gmail.com>
2024-08-01 12:17:11 +0000

    llama 405 prep weights

--- experiments/llama_large/prepare_vllm_weights.sh
+++ experiments/llama_large/prepare_vllm_weights.sh
@@ -1,7 +1,7 @@
 python /workspace/git/fsdp_qlora/scripts/prepare_vllm_weights.py \
 --train_type hqq_dora \
 --infer_type tinygemm \
---dora_safetensors_filename /workspace/models/Meta-Llama-3-70B-Instruct-4bit-DoRA/step_500/model_state_dict.safetensors \
---config_filename /workspace/models/Meta-Llama-3-70B-Instruct-4bit-DoRA/step_500/config.json \
---model_name meta-llama/Meta-Llama-3-70B-Instruct \
---save_dir /workspace/models/Meta-Llama-3-70B-Instruct-4bit-DoRA/step_500/vllm_tinygemm
+--dora_safetensors_filename /workspace/models/llama-3-1-405b-instruct-hqq-4bit/step_250/model_state_dict.safetensors \
+--config_filename /workspace/models/llama-3-1-405b-instruct-hqq-4bit/step_250/config.json \
+--model_name meta-llama/Meta-Llama-3.1-405B-Instruct \
+--save_dir /workspace/models/llama-3-1-405b-instruct-hqq-4bit/step_250/vllm_tinygemm
