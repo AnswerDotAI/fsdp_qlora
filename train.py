355f0be6ae2d5581de5fb0b9d413aded5cf87202
Griffin Adams <ga@answer.ai>
2024-10-31 23:08:44 +0000

    Revert back to correct scheduling of cla_shared_coef.

--- train.py
+++ train.py
@@ -880,7 +880,7 @@ def fsdp_main(local_rank:int, world_size:int, args:Dict):
                 # Set the cla_shared_coef based on training_step / num_training_steps
                 if args["cla_kv_cache_map"] is not None:
                     # TODO return to just current_training_step / num_training_steps
-                    cla_shared_coef = min(1.0, 1.0 - current_training_step / num_training_steps)
+                    cla_shared_coef = min(1.0, current_training_step / num_training_steps)
                     if rank == 0:
                         print(f"Updating CLA shared coef to {cla_shared_coef}")
                         if current_training_step > num_training_steps:
